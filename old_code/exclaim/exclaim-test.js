72da80271c3c438dc08e5df227466ff27557b843
Nikolaos <lunaticnick@hotmail.com>
2018-01-08 14:30:36 +0000

    Setting up the basic structure

---
+++ old_code/exclaim/exclaim-test.js
@@ -0,0 +1,9 @@
+var exclaim = require("./exclaim").exclaim;
+
+if (exclaim("hi") !== "hi!!!!!") {
+  throw new Error("Exclaiming hi should equal hi!!!!!");
+} else {
+  console.log(".");
+}
+console.log(exports);
+console.log("hello");
